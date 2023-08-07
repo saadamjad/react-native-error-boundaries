2c6dfa92955a161610ed431837acab4dd639842b
Muhammad Saad Amjad <46295251+saadamjad@users.noreply.github.com>
2023-08-07 11:41:52 +0500

    Feat: Completed Contact import feature

--- src/screens/main/category/index.tsx
+++ src/screens/main/category/index.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
-import React from 'react';
-import { View, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, FlatList, Text, PermissionsAndroid, Button } from 'react-native';
 import { staticText } from '../../../utils/staticTexts';
 import {
 	Header,
@@ -14,18 +14,56 @@ import {
 	getNewsData,
 	isLoading,
 } from '../../../state/selectors/features/category';
+import Contacts from 'react-native-contacts';
 
 const ProductList = ({ navigation }: ProductListProps) => {
 	const news = useSelector(getNewsData);
 	const isLoader = useSelector(isLoading);
+	const [contact, setContacts] = useState([]);
 
-	const renderItem = ({ item }: { item: Product }) => {
-		return (
-			<ProductCard
-				item={item}
-				navigation={navigation}
-			/>
-		);
+	const renderItem = ({ item, index }: { item: Product }) => {
+		// console.log('===', item?.phoneNumbers[0]?.number);
+		// console.log('===11', index);
+		if (contact?.length >= 1) {
+			return (
+				<View
+					style={{
+						flexDirection: 'row',
+						alignItems: 'center',
+						height: 70,
+						borderBottomWidth: 1,
+						paddingHorizontal: 10,
+					}}>
+					<View
+						style={{
+							height: 40,
+							width: 40,
+							borderRadius: 40,
+							backgroundColor: 'gray',
+						}}></View>
+					<View style={{ paddingHorizontal: 10 }}>
+						<Text style={{ fontSize: 20 }}> {item.givenName} </Text>
+						<Text style={{ fontSize: 20 }}>
+							{item?.phoneNumbers[0]?.number}{' '}
+						</Text>
+					</View>
+				</View>
+			);
+		}
+		// else {
+		// 	return (
+		// 		<Button
+		// 			title='Import Contacts'
+		// 			onPress={importContacts}
+		// 		/>
+		// 	);
+		// }
+		// return (
+		// 	<ProductCard
+		// 		item={item}
+		// 		navigation={navigation}
+		// 	/>
+		// );
 	};
 	const renderLoader = () => {
 		if (isLoader) {
@@ -44,18 +82,59 @@ const ProductList = ({ navigation }: ProductListProps) => {
 	const renderHeader = () => (
 		<Header
 			testID='headerComponent'
-			headerText={staticText.TOP_NEWS}
+			headerText={'contacts'}
 		/>
 	);
+
+	const requestContactsPermission = async () => {
+		try {
+			const granted = await PermissionsAndroid.request(
+				PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+				{
+					title: 'Contacts Permission',
+					message: 'This app requires access to your contacts.',
+					buttonNeutral: 'Ask Me Later',
+					buttonNegative: 'Cancel',
+					buttonPositive: 'OK',
+				}
+			);
+			if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+				console.log('Contacts permission granted');
+			} else {
+				console.log('Contacts permission denied');
+			}
+		} catch (error) {
+			console.log('Error requesting contacts permission:', error);
+		}
+	};
+	useEffect(() => {
+		// requestContactsPermission();
+	}, []);
+
+	const importContacts = () => {
+		Contacts.getAll()
+			.then((contacts) => {
+				console.log('Contacts:', contacts);
+				setContacts(contacts);
+			})
+			.catch((error) => {
+				console.log('Error importing contacts:', error);
+			});
+	};
 	return (
 		<View testID='productParentComponent'>
 			{renderHeader()}
-			{renderFilterBar()}
 			{renderLoader()}
+			{contact?.length === 0 && (
+				<Button
+					title='Import Contacts'
+					onPress={importContacts}
+				/>
+			)}
 			<FlatList
 				testID='flatListComponent'
 				showsVerticalScrollIndicator={false}
-				data={news}
+				data={contact}
 				renderItem={renderItem}
 			/>
 		</View>
